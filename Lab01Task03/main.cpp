cb1b1ff07cf46550294877edaa9dc448c9405cc2
mohammadhossainjoy <joyh2755@gmail.com>
2024-12-26 13:07:42 +0600

    lab1task3 add

---
+++ Lab01Task03/main.cpp
@@ -0,0 +1,17 @@
+/*Task 3: Using the new operator, allocate a two dimensional integer array. The number of rows and columns are going
+to be provided by the user as input. However, in this task, all of the rows are not the same size (the array is uneven).
+The user will specify how many elements the individual rows will have. Assign values to the array elements by taking
+user inputs and then print the values. Finally de-allocate the array using the delete operator.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+
+int row,col;
+cout<<"Enter
+
+
+
+
+return 0;
+}
